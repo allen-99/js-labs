f540b159e4686c80ee232104a6953846ab0560b1
Kruychkova Natalya <kryuchkova_nyu@farpost.com>
2023-10-28 17:23:13 +0300

    add 6 labs

---
+++ js-lab-6.mjs
@@ -0,0 +1,14 @@
+// 6 Предоставьте код async-функции task от одного аргумента x, которая возвращает 
+// в виде установленного промиса (строчное) значение свойства name переданного 
+// ей объекта (это значение само является установленным промисом)
+
+
+async function task(x) {
+  return x.name;
+}
+
+const test = {
+  name: Promise.resolve('parapampampam')
+};
+
+console.log(await task(test));
